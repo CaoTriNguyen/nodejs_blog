b6fcdd0e8864f20a767977b5c51f17e367b17334
CaoTriNguyen <caotri.0696@gmail.com>
2022-09-12 18:23:43 +0700

    ADD: Controller and routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(morgan('combined'));
@@ -17,13 +19,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine','hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
