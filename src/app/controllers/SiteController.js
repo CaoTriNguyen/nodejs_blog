a8f41e967ebe2864f1be969b2ea89977dbb05495
CaoTriNguyen <caotri.0696@gmail.com>
2022-09-12 19:49:06 +0700

    install prettier lint-staged husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,12 @@
-
-class SiteController{
-    // [GET] 
-    index(req, res){       
+class SiteController {
+    // [GET]
+    index(req, res) {
         res.render('news');
     }
     //[GET] /news/search
-    search(req, res){
+    search(req, res) {
         res.render('search');
     }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -7,21 +7,23 @@ const port = 3000;
 
 const route = require('./routes');
 
-
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
 
 //templete engine
-app.engine('hbs', handlebars.engine({ 
-  extname: '.hbs',
-  defaultLayout: 'main'
-}));
-app.set('view engine','hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        defaultLayout: 'main',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
